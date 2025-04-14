0b9443eafdee10f6ae7e11a6a9f14435d9d0c0fa
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-04-14 15:35:13 +0200

    Add data analysis and generation modules with statistics computation

--- experiments/analysis.py
+++ experiments/analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def get_statistics(X: np.ndarray, quantiles: np.ndarray, estimates: np.ndarray) -> float:
@@ -11,17 +12,20 @@ def get_statistics(X: np.ndarray, quantiles: np.ndarray, estimates: np.ndarray) 
     """
     # sort the data
     X = np.sort(X)
+    n = len(X)
 
     # for each result, get the position of the closest element in the sorted array
     positions = np.searchsorted(X, estimates)
-    # compute the rank error
-    rank_errors = np.abs(positions / len(X) - quantiles)
+    true_positions = [math.floor(q * n) for q in quantiles]
+
+    # compute the normalized rank error
+    rank_errors = np.abs(positions - true_positions) / n
 
     # get statistics
     max_rank_error = np.max(rank_errors)
     mean_rank_error = np.mean(rank_errors)
     output = {
-        'max_rank_error': max_rank_error,
+        'max_rank_error':  max_rank_error,
         'mean_rank_error': mean_rank_error
     }
     return output
