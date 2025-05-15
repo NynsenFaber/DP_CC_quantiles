ed400788abf9527e17692cd330dc9b6c56d1cbf9
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-05-15 18:46:20 +0200

    feat: enhance approximate quantiles algorithm with swap option; add real data notebook for experiments

--- DP_AQ/approximate_quantiles_algo.py
+++ DP_AQ/approximate_quantiles_algo.py
@@ -29,12 +29,13 @@ def gaussian_noise(array, bounds, scale=0.00001):
     return np.sort(data + np.random.normal(0, scale, len(array))), (bounds[0] - 4 * scale, bounds[1] + 4 * scale)
 
 
+## MOD, added swap to algo_hepler
 def approximate_quantiles_algo(array, quantiles, bounds, epsilon, swap=False, cdp=False, random_gauss=True):
     epsilon = get_epsilon(len(quantiles), epsilon, swap, cdp)
     if random_gauss:
         array, bounds = gaussian_noise(array, bounds)
 
-    def algo_helper(array, quantiles, bounds):
+    def algo_helper(array, quantiles, bounds, swap):
         m = len(quantiles)
         a, b = bounds
         if m == 0:
@@ -42,14 +43,14 @@ def approximate_quantiles_algo(array, quantiles, bounds, epsilon, swap=False, cd
 
         if m == 1:
             return [
-                single_quantile(array, bounds, quantiles[0], epsilon=epsilon, swap=False)]
+                single_quantile(array, bounds, quantiles[0], epsilon=epsilon, swap=swap)]
 
         q_mid = quantiles[m // 2]
-        v = single_quantile(array, bounds, q_mid, epsilon=epsilon, swap=False)
+        v = single_quantile(array, bounds, q_mid, epsilon=epsilon, swap=swap)
         d_l, d_u = split_by_number(array, v)
         q_l, q_u = np.array_split(quantiles[quantiles != q_mid], 2)
         q_l, q_u = q_l / q_mid, (q_u - q_mid) / (1 - q_mid)
 
-        return algo_helper(d_l, q_l, (a, v)) + [v] + algo_helper(d_u, q_u, (v, b))
+        return algo_helper(d_l, q_l, (a, v), swap) + [v] + algo_helper(d_u, q_u, (v, b), swap)
 
-    return algo_helper(np.sort(array), quantiles, bounds)
+    return algo_helper(np.sort(array), quantiles, bounds, swap)
